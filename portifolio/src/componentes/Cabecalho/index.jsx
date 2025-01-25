2a077533308ad942b8b212d9036f7fae39fdb3c4
felipesora <felipeusora@gmail.com>
2025-01-25 01:21:33 -0300

    Cabecalho finalizado

--- portifolio/src/componentes/Cabecalho/index.jsx
+++ portifolio/src/componentes/Cabecalho/index.jsx
@@ -1,13 +1,20 @@
 import './Cabecalho.css'
 
 const Cabecalho = () => {
+
     return (
         <header className='cabecalho'>
-            <div>
+            <div className='cabecalho_botoes'>
+                <button>Sobre mim</button>
+                <button>Habilidades</button>
+                <button>Projetos</button>
+                <button>Certificados</button>
+            </div>
+            <div className='boas_vindas'>
                 <h3>OLÁ, SEJA BEM VINDO!</h3>
                 <h1>Eu sou Felipe Sora</h1>
                 <h2>Engenheiro de Software | Desenvolvedor Web | Estudante de ADS</h2>
-                <button>SOBRE MIM <img src="/imagens/seta-baixo-32px.png" alt="Seta para baixo" /></button>
+                <button className='botao_sobre_mim'>SOBRE MIM <img src="/imagens/seta-baixo-branca-32px.png" alt="Seta para baixo" /></button>
             </div>
         </header>
     )
