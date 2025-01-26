b97c7c4196a20fadbb524d189e5bbc37459c5ab4
felipesora <felipeusora@gmail.com>
2025-01-26 03:22:52 -0300

    Criando card de forma dinâmica

--- portifolio/src/componentes/CardHabilidade/index.jsx
+++ portifolio/src/componentes/CardHabilidade/index.jsx
@@ -1,10 +1,12 @@
 import './CardHabilidade.css'
 
-const CardHabilidade = () => {
+const CardHabilidade = (props) => {
     return (
         <div className='card_habilidade'>
-            <img src="/imagens/python.png" alt="" />
-            <h3>Python</h3>
+            <img src={props.iconeHabilidade} alt={props.nomeHabilidade} />
+            <h3>{props.nomeHabilidade}</h3>
+            <img src={props.barraHabilidade} alt={props.nivelHabilidade} className='img_nivel_habilidade'/>
+            <p>{props.nivelHabilidade}</p>
         </div>
     )
 }
--- portifolio/src/componentes/Habilidades/index.jsx
+++ portifolio/src/componentes/Habilidades/index.jsx
@@ -2,11 +2,40 @@ import CardHabilidade from '../CardHabilidade';
 import './Habilidades.css'
 
 const Habilidades = () => {
-    return(
+    return (
         <div id='habilidades'>
             <h2>HABILIDADES</h2>
-            <p>Nesta seção, apresento as principais tecnologias e ferramentas com as quais trabalho. Cada habilidade é exibida com meu nível de experiência, refletindo meu conhecimento e prática em cada uma delas. Desde linguagens de programação até frameworks modernos, estou sempre aprendendo e me atualizando para entregar as melhores soluções.</p>
-            <CardHabilidade />
+            <p className='textos'>Nesta seção, apresento as principais tecnologias e ferramentas com as quais trabalho. Cada habilidade é exibida com meu nível de experiência, refletindo meu conhecimento e prática em cada uma delas. Desde linguagens de programação até frameworks modernos, estou sempre aprendendo e me atualizando para entregar as melhores soluções.</p>
+            <CardHabilidade
+            iconeHabilidade={'/imagens/python.png'}
+            nomeHabilidade='Python'
+            barraHabilidade='/imagens/muito-bom.png'
+            nivelHabilidade='muito bom'
+            />
+            <CardHabilidade
+            iconeHabilidade={'/imagens/javascript.png'}
+            nomeHabilidade='JavaScript'
+            barraHabilidade='/imagens/muito-bom.png'
+            nivelHabilidade='muito bom'
+            />
+            <CardHabilidade
+            iconeHabilidade={'/imagens/react.png'}
+            nomeHabilidade='React'
+            barraHabilidade='/imagens/intermediario.png'
+            nivelHabilidade='intermediário'
+            />
+            <CardHabilidade
+            iconeHabilidade={'/imagens/html.png'}
+            nomeHabilidade='HTML'
+            barraHabilidade='/imagens/avancado.png'
+            nivelHabilidade='avançado'
+            />
+            <CardHabilidade
+            iconeHabilidade={'/imagens/css.png'}
+            nomeHabilidade='CSS'
+            barraHabilidade='/imagens/muito-bom.png'
+            nivelHabilidade='muito bom'
+            />
         </div>
     )
 }
