141e2d2e4bb27d9c6da0a1d316c49d4692a774cf
felipesora <felipeusora@gmail.com>
2025-01-27 00:13:29 -0300

    Cards de projetos finalizados

--- portifolio/src/componentes/CardProjeto/index.jsx
+++ portifolio/src/componentes/CardProjeto/index.jsx
@@ -3,12 +3,13 @@ import './CardProjeto.css'
 const CardProjeto = () => {
     return(
         <div className='card_projeto'>
-            <img src="../../../public/imagens/projeto-smart-energy.png" alt="" />
+            <img src="../../../public/imagens/projeto-alura-plus.png" alt="" />
             <h2>SmartEnergy</h2>
             <p>Este projeto é uma aplicação web desenvolvida com React e Node.js.</p>
-            <h3>Links:</h3>
-            <a href="#">Repositório Github</a>
-            <a href="#">Link do projeto</a>
+            <ul>
+                <a href="#">Repositório Github</a>
+                <a href="#">Link do projeto</a>
+            </ul>
         </div>
     )
 }
--- portifolio/src/componentes/Projetos/index.jsx
+++ portifolio/src/componentes/Projetos/index.jsx
@@ -6,7 +6,7 @@ const Projetos = () => {
         <div id='projetos'>
             <h1>PROJETOS</h1>
             <p className='texto_projetos'>Aqui estão alguns dos projetos que desenvolvi. Eles incluem links para os repositórios ou sites, onde você pode conferir mais detalhes sobre as soluções que construí.</p>
-            <div>
+            <div className='div_projetos'>
                 <CardProjeto />
             </div>
         </div>
