0e3a869237b28b90755d61d65b83550d24cce7f1
felipesora <felipeusora@gmail.com>
2025-01-28 16:32:48 -0300

    Arrumando css botão para subir

--- portifolio/src/componentes/BotaoParaCima/index.jsx
+++ portifolio/src/componentes/BotaoParaCima/index.jsx
@@ -1,19 +1,37 @@
-import { useEffect, useState } from 'react'
-import './BotaoParaCima.css'
+import React, { useState, useEffect } from 'react';
+import './BotaoParaCima.css';
 
 const BotaoParaCima = () => {
+    const [isTop, setIsTop] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY === 0) {
+                setIsTop(true);
+            } else {
+                setIsTop(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const scrollToTop = () => {
-        window.scrollTo({top: 0, behavior: "smooth"});
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
-    return(
-        <button 
-        onClick={scrollToTop}
-        className="btn_scroll-to-top">
+    return (
+        <button
+            onClick={scrollToTop}
+            className={`btn_scroll-to-top ${!isTop ? 'show' : ''}`}
+        >
             <img src="/imagens/seta-para-cima.png" alt="seta para cima" />
         </button>
-    )
-}
+    );
+};
 
-export default BotaoParaCima
+export default BotaoParaCima;
