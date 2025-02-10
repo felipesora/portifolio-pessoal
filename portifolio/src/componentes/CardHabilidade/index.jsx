85a8106e35ff0e2ef128b285f72d737429680fe0
felipesora <felipeusora@gmail.com>
2025-02-10 12:33:03 -0300

    Responsividade do componente Habilidades

--- portifolio/src/componentes/CardHabilidade/index.jsx
+++ portifolio/src/componentes/CardHabilidade/index.jsx
@@ -3,7 +3,7 @@ import './CardHabilidade.css'
 const CardHabilidade = (props) => {
     return (
         <div className='card_habilidade'>
-            <img src={props.iconeHabilidade} alt={props.nomeHabilidade} />
+            <img src={props.iconeHabilidade} alt={props.nomeHabilidade} className='icone_habildade'/>
             <h3>{props.nomeHabilidade}</h3>
             <img src={props.barraHabilidade} alt={props.nivelHabilidade} className='img_nivel_habilidade'/>
             <p>{props.nivelHabilidade}</p>
